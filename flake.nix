ea1c9b6d3d4196d39f38de5ff2a5ff5905878bc1
Sergei Mironov <sergei.v.mironov@proton.me>
2024-09-13 21:00:51 +0400

    Update

--- flake.nix
+++ flake.nix
@@ -6,16 +6,45 @@
     };
   };
 
-  outputs = { self, nixpkgs }: rec {
+  outputs = { self, nixpkgs }: let
+    inherit (nixpkgs.lib) nixosSystem;
+    raspi-nixos = self.outputs.nixosConfigurations.raspi;
+    system-host = "x86_64-linux";
+    system-target = "aarch64-linux";
+    pkgs = import nixpkgs { system = system-host; };
+  in rec {
+
     packages = {
-      x86_64-linux = {
-        sdimage = nixosConfigurations.raspi4.config.system.build.sdImage;
+      x86_64-linux = rec {
+
+        sdimage = raspi-nixos.config.system.build.sdImage;
+
+        uboot3 = raspi-nixos.pkgs.ubootRaspberryPi3_64bit;
+
+        qemu-raspi3 = pkgs.writeScript "qemu-raspi3" ''
+          #!${pkgs.runtimeShell}
+
+          set -e -x
+          IMG=_aarch64-qemu.img
+          cp -f ${sdimage}/*/*img "$IMG"
+          chmod 0640 "$IMG"
+          ${pkgs.qemu}/bin/qemu-img resize -f raw "$IMG" 4G
+
+          ${pkgs.qemu}/bin/qemu-system-aarch64 \
+            -machine raspi3b \
+            -kernel "${uboot3}/u-boot.bin" \
+            -drive file="$IMG",format=raw \
+            -device usb-net,netdev=net0 \
+            -netdev user,id=net0,hostfwd=tcp::22422-:22 \
+            -serial null \
+            -serial mon:stdio
+        '';
       };
     };
 
     nixosConfigurations = {
-      raspi4 = nixosSystem {
-        system = "aarch64-linux";
+      raspi = nixosSystem {
+        system = system-target;
         modules = [
           "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
           ./configuration.nix
